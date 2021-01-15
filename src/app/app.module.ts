7fd0162da7562523e0cffdba894254389e2003d5
saurav004 <logtosaurav@gmail.com>
2021-01-15 09:37:06 +0530

    Add:welcome to Fundoo App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
+import { RegisterComponent } from './register/register.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BreakPointRegistry, FlexLayoutModule, FlexStyleBuilder, LayoutAlignStyleBuilder, LayoutGapStyleBuilder, LayoutStyleBuilder, MediaMarshaller, PrintHook, StylesheetMap, StyleUtils, ɵMatchMedia } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatCardModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatCardModule,
   ],
-  providers: [],
+  providers: [
+    PrintHook,
+    StyleUtils, 
+    StyleSheet,
+    StylesheetMap, 
+    LayoutAlignStyleBuilder,
+    LayoutStyleBuilder,
+    FlexStyleBuilder,
+    MediaMarshaller,
+    ɵMatchMedia,
+    BreakPointRegistry,
+    LayoutGapStyleBuilder
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
