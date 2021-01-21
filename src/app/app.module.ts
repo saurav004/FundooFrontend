718fffc54b18acff9fb0744ba7ef4fdbc55d156e
saurav004 <logtosaurav@gmail.com>
2021-01-21 08:01:21 +0530

    Add:registration form controls and methods created in TS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BreakPointRegistry, FlexLayoutModule, FlexStyleBuilder, LayoutAlignStyleBuilder, LayoutGapStyleBuilder, LayoutStyleBuilder, MediaMarshaller, PrintHook, StylesheetMap, StyleUtils, ɵMatchMedia } from '@angular/flex-layout';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { MatInputModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatCardModule} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material/checkbox'; 
+import { UserService } from './user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
@@ -24,8 +31,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatIconModule,
     MatFormFieldModule,
     MatCardModule,
+    MatCheckboxModule,
+    AppRoutingModule,
   ],
   providers: [
+    UserService,
     PrintHook,
     StyleUtils, 
     StyleSheet,
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
+import { UserService } from './../user.service';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Registeration } from '../models/Registeration';
+
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  hide = true;
+  doesPasswordMatch = false;
+  registration: Registeration = new Registeration();
+  firstname = new FormControl(this.registration.firstName, [Validators.required, Validators.minLength(3), Validators.maxLength(60)]);
+  lastname = new FormControl(this.registration.lastName, [Validators.required, Validators.minLength(3), Validators.maxLength(60)]);
+  email = new FormControl(this.registration.email, [Validators.required, Validators.email]);
+  password = new FormControl(this.registration.password, [Validators.required, Validators.minLength(8), Validators.maxLength(15)]);
+  confirmPassword = new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(15)]);
+  phoneNumber = new FormControl(this.registration.phoneNumber, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]);
 
-  constructor() { }
+  constructor(private _userService: UserService) {}
 
   ngOnInit() {
   }
 
+  firstnameValidation() {
+    return this.firstname.hasError('required') ? 'field is required' : '';
+  }
+  lastNameValidation() {
+    return this.lastname.hasError('required') ? 'field is required' : '';
+  }
+
+  emailValidation() {
+    return this.email.hasError('required') ? 'field is required' :
+      this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  getErrorPhoneNumber() {
+    return this.phoneNumber.hasError('required') ? 'field is required' :
+      this.phoneNumber.hasError('phoneNumber') ? '10 characters required' : '';
+  }
+  getErrorPassword() {
+    return this.password.hasError('required') ? 'field is required' :
+      this.password.hasError('password') ? 'min 8 elements' : '';
+  }
+  getErrorConfirmPassword() {
+    return this.confirmPassword.hasError('required') ? 'field is required' :
+      this.confirmPassword.hasError('confirmPasword') ? 'min 8 characters' : '';
+  }
+  Register() {
+    console.log(this.registration);
+    this._userService.sinup(this.registration)
+      .subscribe(
+        data => console.log('Success!', data),
+        error => console.error('Error!', error)
+      )
+  }
+
+  // confirm new password validator
+  private passwordMatcher() {
+    if(this.confirmPassword == this.password){
+      this.doesPasswordMatch = true;
+    }
+    else
+    this.doesPasswordMatch = false;
+  }
 }
